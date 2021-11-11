a6d23265723677e4a4826b1d8e6c91489b9c64df
unknown <alfahrimudzik@gmail.com>
2021-11-11 21:35:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { NavLink, Route, Routes, Navigate } from "react-router-dom";
+import "./App.css";
+import { CgHome } from "react-icons/cg";
+import { HiMenuAlt2 } from "react-icons/hi";
+import { MdOutlineAccountCircle } from "react-icons/md";
+import { HiSearch } from "react-icons/hi";
+import { BsBookmark } from "react-icons/bs";
+import { CgMenuRound } from "react-icons/cg";
+import Movie from "./movie";
+import Profile from "./profile";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <div className="containerhead">
+          <HiMenuAlt2 className="menuicon" />
+          <HiSearch className="menuicon" />
+        </div>
       </header>
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Navigate replace to="/movie" />} />
+          <Route path="/movie" element={<Movie />}></Route>
+          <Route path="/movies" element={<Profile />}></Route>
+        </Routes>
+        <footer>
+          <div className="containernav">
+            <NavLink to="/movie" className="iconwrapper">
+              <CgHome className="icon" />
+            </NavLink>
+            <NavLink to="/none" className="iconwrapper">
+              <CgMenuRound className="icon" />
+            </NavLink>
+            <NavLink to="/none" className="iconwrapper">
+              <BsBookmark className="icon" />
+            </NavLink>
+            <NavLink to="/profile" className="iconwrapper">
+              <MdOutlineAccountCircle className="icon" />
+            </NavLink>
+          </div>
+        </footer>
+      </div>
     </div>
   );
 }
---
+++ src/profile.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./profile.css";
+
+function Profile() {
+  return (
+    <div className="con">
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+      <h1>hello</h1>
+    </div>
+  );
+}
+export default Profile;
